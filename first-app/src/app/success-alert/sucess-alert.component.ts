e989abb447cf8a4939ddddfce7aed357bcc9e70d
beachf1res <lisaxbarkar@gmail.com>
2023-05-19 22:05:52 +0300

    1st assignment

---
+++ first-app/src/app/success-alert/sucess-alert.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success',
+  templateUrl: './success-alert.component.html'
+})
+
+export class SuccessAlertComponent {}
---
+++ first-app/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning',
+  templateUrl: './warning-alert.component.html',
+  styleUrls: ['./warning-alert.component.css'],
+})
+
+export class WarningAlertComponent {}
